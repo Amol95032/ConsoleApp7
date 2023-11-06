df17abcd44f5dfcfc9a82502ac44a89f3b0929a3
Admin <Admin@DESKTOP-EPVV5M1>
2023-11-06 08:13:13 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int spc, len = 1, count;
+            Console.WriteLine("Enter the Number");
+            count=Convert.ToInt32(Console.ReadLine());
+            spc = count - 1;
+            for (int i = 1; i <= count; i++)
+            {
+                for (int r = 1; r <= spc; r++)
+                {
+                    Console.WriteLine(" ");
+                }
+            }
+            for(int s=1; s <= len; s++)
+            {
+                Console.Write("*");
+            }
+            spc--;
+            len = len + 2;
+            Console.WriteLine();
+        }
+         
+    }
+
+}
